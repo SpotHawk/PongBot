e8f1cac022732c157f97640db7689ac64aee491e
Bz0l1 <90034118+Bz0l1@users.noreply.github.com>
2022-08-01 16:51:45 +0200

    Kihív parancs adatbázis lekérdezéssel

--- main.py
+++ main.py
@@ -7,25 +7,52 @@ load_dotenv()
 
 client = discord.Client()
 
-mydb = mysql.connector.connect(host=os.getenv('HOST'),user=os.getenv('DBU'),password=os.getenv('DBP'),database='PRx5iD5O8U')
-mycursor=mydb.cursor()
+mydb = mysql.connector.connect(host=os.getenv('HOST'), user=os.getenv('DBU'), password=os.getenv('DBP'),
+                               database=os.getenv('DBN'))
+mycursor = mydb.cursor()
 
 mycursor.execute('select users.dcid from users')
 
-ids=[]
+
+class Users:
+    def __init__(self, dcnev, dcid, pont, coin):
+        self.dcnev = dcnev
+        self.dcid = dcid
+        self.pont = pont
+        self.coin = coin
+
+
+ids = []
 for item in mycursor:
     ids.append(str(item).strip("('',)"))
+print(ids)
 
-class KihivClass:
-    def __init__(self, authorName, authorId, kihivNev, kihivId):
-        self.authorName = authorName
-        self.authorId = authorId
-        self.kihivNev = kihivNev
-        self.kihivId = kihivId
+
+adatokDCNEV, adatokDCID, adatokPONT, adatokCOIN = [], [], [], []
+mycursor.execute('SELECT users.dcnev FROM users')
+for item in mycursor:
+    adatokDCNEV.append(str(item).strip("('',)"))
+mycursor.execute('SELECT users.dcid FROM users')
+for item in mycursor:
+    adatokDCID.append(str(item).strip("('',)"))
+mycursor.execute('SELECT users.pont FROM users')
+for item in mycursor:
+    adatokPONT.append(str(item).strip("('',)"))
+mycursor.execute('SELECT users.coin FROM users')
+for item in mycursor:
+    adatokCOIN.append(str(item).strip("('',)"))
+
+adatok = Users(adatokDCNEV, adatokDCID, adatokPONT, adatokCOIN)
+print(adatok.dcnev)
+print(adatok.dcid)
+print(adatok.pont)
+print(adatok.coin)
+print(adatok.dcnev[1])
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -33,22 +60,16 @@ async def on_message(message):
         return
 
     if message.content.startswith('ping'):
-        uid=str(message.author.id)
+        uid = str(message.author.id)
         if uid not in ids:
             sql = "INSERT INTO users (dcnev, dcid, pont, coin) VALUES (%s, %s, %s, %s)"
-            val = (message.author.name, message.author.id, 0,0)
+            val = (message.author.name, message.author.id, 0, 0)
             mycursor.execute(sql, val)
             mydb.commit()
             ids.append(str(message.author.id))
-            return
+
         if 'kihiv' in message.content:
-            user1 = message.author.id
-            #kihivtomb = [user1, message.author.name, str(232185184739524608), 'SpotHawk']
-            kihivott = KihivClass(message.author.id, message.author.name, 'SpotHawk', str(232185184739524608))
-            if kihivott.kihivNev in message.content:
-                await message.channel.send(f"<@{kihivott.kihivId}>")
-            print(message.author.display_name)
-        else:
-            await message.channel.send('Hello!')
-            
+            nev = message.content[11:]
+            if nev in adatok.dcnev:
+                await message.channel.send(f'<@{message.author.id}> kihívott téged, <@{adatok.dcid[1]}>')
 client.run(os.getenv('TOKEN'))
